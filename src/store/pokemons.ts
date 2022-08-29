c5f07ee8324eb1f60d7aa19ce2eb6ae331a914f6
Romain 'Ashley' Bonhomme <romain.bonhomme89@gmail.com>
2022-08-29 14:02:09 -0400

    Update pokemons.ts

--- src/store/pokemons.ts
+++ src/store/pokemons.ts
@@ -3,7 +3,7 @@ import { ref, computed } from 'vue';
 import { PokeAPIBaseResult } from '../types'
 import data from '../assets/data.json';
 
-export const usePokeStore = defineStore('team', () => {
+export const usePokeStore = defineStore('pokemons', () => {
 	const pokemons = computed<PokeAPIBaseResult[]>(() => data.data);
 	const limit = ref(20);
 	const APIURL = computed(() => `https://pokeapi.co/api/v2/pokemon?limit=${limit.value}&offset=0`)
