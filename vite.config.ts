f1198f89e3cb03fd172fe26085089abec7aee41b
ROMAIN BONHOMME <rbonhomme@exolnet.com>
2024-09-20 16:46:49 -0400

    chore: update packages and config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // or "modern"
+      }
+    }
+  },
   plugins: [vue()]
 })
